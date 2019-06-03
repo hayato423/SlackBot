815b132614995e09a99187be94358ba09618ac38
hayato423 <49159244+hayato423@users.noreply.github.com>
2019-06-03 14:31:07 +0900

    firstcomit

---
+++ slackbot/slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-77012139186-641047493138-egYY0ps2JKL4blWcBXtQ48Lz"
+
+#このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = 'ちょっと何言ってるかわからない'
+
+#プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
